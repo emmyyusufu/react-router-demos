1a8cb391d371c93870e57cb3b314fe666193bdc4
Emmanuel Yusufu <emmayusufu@gmail.com>
2017-11-27 13:18:18 +0100

    initial file addition

---
+++ 01-basic-routing/src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+// import route Components here
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <div className="container">
+          <ul>
+            <li><a href="">Hello</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Books</a></li>
+          </ul>
+          <hr/>
+
+          {/* Routes will go here */}
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 01-basic-routing/src/components/HelloComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+Hello = () => {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-3">Hello, world!</h1>
+        </div>
+    );
+}
+
+export default Hello;
