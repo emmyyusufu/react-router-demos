d5520fb8986971be701c90ae0035a009c9cec683
Emmanuel Yusufu <emmayusufu@gmail.com>
2017-11-28 06:25:02 +0100

    demo three final

--- 03-nested-routing-with-path-parameters/src/components/BooksComponent.js
+++ 03-nested-routing-with-path-parameters/src/components/BooksComponent.js
@@ -27,12 +27,20 @@ const Books = ({ match }) => {
                     <Route path={`${match.path}/html`} render={() => { return <h1>HTML by Ducket book</h1> }}/>
                     <Route path={`${match.path}/css`} render={() => { return <h1>CSS by Racheal Andrews</h1> }}/>
                     <Route path={`${match.path}/react`} render={() => { return <h1>React by Fullstack.io book</h1> }}/>
+                    <Route path={`${match.path}/:id`} component={Child} />
                 </div>
             </div>
             </div>
         </div>
     );
 }
+
+const Child = ({ match }) => (
+    <div>
+      <h3>URL ID parameter: {match.params.id}</h3>
+    </div>
+);
+
 
 export default Books;
 
